e4c10e003ae017c84f3aaae89f57322e66b6e87a
Taylor T <taylorturnerit@gmail.com>
2023-10-30 13:40:09 -0500

    Begun encapsulation of draw methods

--- screen.py
+++ screen.py
@@ -1,15 +1,20 @@
 import pygame
+import logging
+from math import floor
 
 # VIEW OBJECT
 # TODO: Create an object to store draw methods w/ preferred layer
+
+# This system is a layer based approach, with only one draw per layer. Perhaps in the future I can investigate parallel programming which seems like the best approach to drawing two things on the same layer
 class Screen:
-    class drawModule:
-        def __init__(objectToDraw, preferredLayer, layerCount):
-            drawObject = objectToDraw
-            drawObject = preferredLayer
+    # Stores the object and their layer
+    class Module:
+        def __init__(objectToDraw, preferredLayer):
+            obj = objectToDraw
+            layer = preferredLayer
 
     # Input of to caption text, horizontal width, vertical height
-    def __init__(self, captionInput = "My Program", screenWidthInput = pygame.display.Info.x/2, screenHeightInput = pygame.display.Info.y/2):
+    def __init__(self, captionInput = "My Program", screenWidthInput = floor(pygame.display.Info.x/2), screenHeightInput = floor(pygame.display.Info.y/2)):
         # Defines the pygame canvas
         self.canvas = pygame.display.set_mode((screenWidthInput, screenHeightInput)) 
         # Stores all of the programs visual functions in order of layers
@@ -26,7 +31,8 @@ class Screen:
         self.captionText = newCaption
         pygame.display.set_caption(self.captionText)
 
+    # Primary screen update
     def draw(self):
         for x in range(0, self.drawMethods.size):
-            self.drawMethods[x].draw(self.canvas)
-        pygame.display.update() 
+            self.drawMethods.pop().draw(self.canvas)
+        pygame.display.update()
