b482f78fa36c3d9f2d928da1dbed6230ac61b54e
Taylor T <taylorturnerit@gmail.com>
2023-10-31 13:54:56 -0500

    Implementing layer based sorting

--- screen.py
+++ screen.py
@@ -8,7 +8,7 @@ from math import floor
 # This system is a layer based approach, with only one draw per layer. Perhaps in the future I can investigate parallel programming which seems like the best approach to drawing two things on the same layer
 class Screen:
     # Stores the object and their layer
-    class Module:
+    class Node:
         def __init__(objectToDraw, preferredLayer):
             obj = objectToDraw
             layer = preferredLayer
@@ -23,16 +23,31 @@ class Screen:
         self.captionText = captionInput
 
     # Adds a draw function to the draw array, takes an input of an object
-    def addDraw(self, objectToDraw):
-        self.draw.append(objectToDraw)
+    def addDraw(self, objectToDraw, preferredLayer):
+        self.newNode = self.Node(objectToDraw, preferredLayer)
+        self.draw.append(self.newNode)
 
     # Externally accessible caption update
     def updateCaption(self, newCaption):
         self.captionText = newCaption
         pygame.display.set_caption(self.captionText)
 
+    def sort(self, unsortedArray):
+        array = unsortedArray
+        i = 1
+        while (i < array.length):
+            j = i
+            while (j > 0 and array[j-1] > array[j]):
+                dummy = array[j]
+                array[j] = array[j-1]
+                array[j-1] = dummy
+                j -= 1
+            i += 1
+
     # Primary screen update
     def draw(self):
+        # Sorts the list by layer each iteration, probably inefficient. TODO: Make better solution
+        self.sort(self.drawMethods)
         for x in range(0, self.drawMethods.size):
             self.drawMethods.pop().draw(self.canvas)
         pygame.display.update()
