3a59df6aaa0f6b22c186fdf6ed490285614759f7
TaylorTurnerIT <tjturner44@tntech.edu>
2023-11-02 12:13:17 -0500

    Implemented layer-based draw sys

--- graph.py
+++ graph.py
@@ -1,11 +1,36 @@
 import pygame
 import numpy as np
 from screen import *
-  
-pygame.init() 
-  
-exit = False
-screen = Screen("Default Program")
+
+# INIT
+pygame.init() # Pygame init
+screen = Screen("Default Program") # Screen init
+exit = False # Game loop exit condition
+
+# Node class for graph
+class Node:
+    def __init__(self, x, y, width, height, color):
+        # Visual properties
+        self.x = x
+        self.y = y
+        self.w = width
+        self.h = height
+        self.c = color
+        # Data properties
+        self.neighbors = []
+        self.data = None
+
+
+    def draw(self, canvas):
+        pygame.draw.rect(canvas, self.c, pygame.Rect(self.x, self.y, self.w, self.h))
+
+
+# GAME VARIABLES
+testRect = Node(0, 0, 100, 100, (0, 155, 0))
+testRect2 = Node(50, 50, 100, 100, (0, 0, 155))
+
+screen.addDraw(testRect, 1)
+screen.addDraw(testRect2, 0)
 
 # GAME LOOP
 while not exit: 
@@ -13,3 +38,6 @@ while not exit:
         if event.type == pygame.QUIT: 
             exit = True
     screen.draw()
+
+# QUIT
+pygame.quit()
--- screen.py
+++ screen.py
@@ -2,6 +2,8 @@ import pygame
 import logging
 from math import floor
 
+pygame.init()
+
 # VIEW OBJECT
 # TODO: Create an object to store draw methods w/ preferred layer
 
@@ -9,12 +11,15 @@ from math import floor
 class Screen:
     # Stores the object and their layer
     class Node:
-        def __init__(objectToDraw, preferredLayer):
-            obj = objectToDraw
-            layer = preferredLayer
+        def __init__(self, objectToDraw, preferredLayer):
+            self.obj = objectToDraw
+            self.layer = preferredLayer
+        # overload < operator
+        def __lt__(self, other):
+            return self.layer < other.layer
 
     # Input of to caption text, horizontal width, vertical height
-    def __init__(self, captionInput = "My Program", screenWidthInput = floor(pygame.display.Info.x/2), screenHeightInput = floor(pygame.display.Info.y/2)):
+    def __init__(self, captionInput = "My Program", screenWidthInput = floor(pygame.display.Info().current_w/2), screenHeightInput = floor(pygame.display.Info().current_h/2)):
         # Defines the pygame canvas
         self.canvas = pygame.display.set_mode((screenWidthInput, screenHeightInput)) 
         # Stores all of the programs visual functions in order of layers
@@ -25,7 +30,9 @@ class Screen:
     # Adds a draw function to the draw array, takes an input of an object
     def addDraw(self, objectToDraw, preferredLayer):
         self.newNode = self.Node(objectToDraw, preferredLayer)
-        self.draw.append(self.newNode)
+        self.drawMethods.append(self.newNode)
+        # Sorts the draw array by layer after new layer is added
+        self.sort(self.drawMethods)
 
     # Externally accessible caption update
     def updateCaption(self, newCaption):
@@ -35,7 +42,7 @@ class Screen:
     def sort(self, unsortedArray):
         array = unsortedArray
         i = 1
-        while (i < array.length):
+        while (i < len(array)):
             j = i
             while (j > 0 and array[j-1] > array[j]):
                 dummy = array[j]
@@ -46,8 +53,6 @@ class Screen:
 
     # Primary screen update
     def draw(self):
-        # Sorts the list by layer each iteration, probably inefficient. TODO: Make better solution
-        self.sort(self.drawMethods)
-        for x in range(0, self.drawMethods.size):
-            self.drawMethods.pop().draw(self.canvas)
+        for x in range(0, len(self.drawMethods)):
+            self.drawMethods[x].obj.draw(self.canvas)
         pygame.display.update()
